c0fd7905523bf90c006ec29205772c04b24391ff
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2022-04-07 20:24:28 -0300

    Update saveVisit.php

--- saveVisit.php
+++ saveVisit.php
@@ -4,9 +4,21 @@
 
 	$connect = mysqli_connect($host, $db_username, $db_password, $db_name);
 
-    $query = "UPDATE mementoMoriUserCount SET visitNumber = visitNumber + 1";
+	$queryGetData = "SELECT * FROM mementoMoriUserCount WHERE 1 = 1";
 
-    $result = mysqli_query($connect,$query);
+    $resultGetData = mysqli_query($connect, $queryGetData);
+
+	if ($rows = mysqli_fetch_array($result)) {
+		$requestNumber = $rows['visitNumber'];
+	}
+
+	$_SERVER['REMOTE_ADDR'];
+
+	{"data": {"date": {"ip": "", "ip2": ""}}}
+
+    $query = "UPDATE mementoMoriUserCount SET visitNumber =  + 1";
+
+    $result = mysqli_query($connect, $query);
 
 	mysqli_close($connect);
 ?>
