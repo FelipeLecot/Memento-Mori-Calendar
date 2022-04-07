ee68937559a7c3a6bb008973a6e03bd5fa3f745b
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2022-04-07 20:34:43 -0300

    Update saveVisit.php

--- saveVisit.php
+++ saveVisit.php
@@ -4,21 +4,50 @@
 
 	$connect = mysqli_connect($host, $db_username, $db_password, $db_name);
 
-	$queryGetData = "SELECT * FROM mementoMoriUserCount WHERE 1 = 1";
-
-    $resultGetData = mysqli_query($connect, $queryGetData);
-
-	if ($rows = mysqli_fetch_array($result)) {
-		$requestNumber = $rows['visitNumber'];
-	}
-
+	
 	$_SERVER['REMOTE_ADDR'];
 
-	{"data": {"date": {"ip": "", "ip2": ""}}}
+	// {"data": {"date": {"ip": "", "ip2": ""}}}
 
-    $query = "UPDATE mementoMoriUserCount SET visitNumber =  + 1";
 
-    $result = mysqli_query($connect, $query);
+	function addVisitToTotalCount() {
+		global $connect;
+		function getCurrentVisitData($connect) {
+
+			$query = "SELECT * FROM mementoMoriUserCount WHERE 1 = 1";
+
+			$result = mysqli_query($connect, $query);
+
+			if ($rows = mysqli_fetch_array($result)) {
+				return json_decode($rows['visitData'], true);
+			}
+		}
+
+		function addNewUniqueVisit() {
+			$query = "UPDATE mementoMoriUserCount SET uniqueVisitNumber = visitNumber + 1";
+	
+			$result = mysqli_query($connect, $query);
+		}
+
+		$currentVisitData = getCurrentVisitData($connect);
+
+		if ($currentRque)
+
+		$query = "UPDATE mementoMoriUserCount SET visitData = $newVisitData + 1";
+
+		$result = mysqli_query($connect, $query);
+	}
+
+	function addVisitData() {
+		global $connect;
+
+		$query = "UPDATE mementoMoriUserCount SET visitNumber = visitNumber + 1";
+
+		$result = mysqli_query($connect, $query);
+	}
+
+	addVisitToTotalCount();
+	addVisitData();
 
 	mysqli_close($connect);
 ?>
